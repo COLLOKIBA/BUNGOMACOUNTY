5c608cbcd6d31d36fb9b8809ce7149d6a0f8bacb
COLLOKIBA <ckibaba6@gmail.com>
2024-08-07 11:28:13 +0300

    second update

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,7 +3,7 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { useEffect, useState } from 'react';
-import styles from './navbar.module.css';
+import styles from './Navbar.module.css';
 import Dropdown from './DropdownMenu';
 import DropdownAbout from './DropdownAbout';
 
