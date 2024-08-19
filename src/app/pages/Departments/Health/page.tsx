76c1973ea7c54f4d5c182212de3b15d1878d4d73
COLLOKIBA <ckibaba6@gmail.com>
2024-08-19 12:50:41 +0300

    correction on the departments

--- src/app/pages/Departments/Health/page.tsx
+++ src/app/pages/Departments/Health/page.tsx
@@ -1,53 +1,4 @@
-// import React from 'react';
-// import DepartmentDescription from '../../../components/DepartmentDescription';
-// import Topbar from '../../../components/Topbar';
-// import Navbar from '../../../components/Navbar';
-// import Footer from '../../../components/Footer';
-
-// const DepartmentPage: React.FC = () => {
-//   const text = `
-//     This is the first line.
-//     This is the second line.
-//     This is the third line.
-//   `;
-  // const ChiefOfficerData = {
-  //   name: 'Dr. Magrina Mayama',
-  //   position: 'Chief Officer, Health Department',
-  //   email: '@example.com',
-  //   phone: '+254',
-  //   description: 'is responsible for overseeing the ...',
-  //   imageUrl: '/chiefhealth.jpg',
-  // };
- 
-  // const CECMData = {
-  //   name: ' Ms. Carolyne Makali Khalayi',
-  //   position: 'County Executive Committee Member, Health Department',
-  //   email: 'jane.smith@example.com',
-  //   phone: '+254',
-  //   description:(
-  //     <div>
-  //       <p>
-  //         Ms. Carolyne Makali Khalayi hails from the Luhya tribe and is a proud resident of Kabuchai Constituency. 
-  //         She brings a wealth of experience and dedication to the Health Department.
-  //       </p>
-  //       <p>
-  //         <strong>Educational Background:</strong>
-  //       </p>
-  //       <ul>
-  //         <li>Bachelor's Degree in Business Commerce – Accounting Option from Kibabii University.</li>
-  //       </ul>
-  //       <p>
-  //         <strong>Professional Experience:</strong>
-  //       </p>
-  //       <ul>
-  //         <li>Former Records Management Officer at the National Government Constituency Development Fund, Kabuchai Constituency.</li>
-  //         <li>Served as a Research Assistant at the National Taxpayers’ Association.</li>
-  //       </ul>
-  //     </div>
-  //   ),
-  //   imageUrl: '/carolynemakalakhalayi.jpg',
-  // };
-
+/* eslint-disable react/no-unescaped-entities */
 
 import React from 'react';
 import DepartmentDescription from '../../../components/DepartmentDescription';
@@ -91,14 +42,14 @@ const DepartmentPage: React.FC = () => {
           <strong>Educational Background:</strong>
         </p>
         <ul>
-          <li>Bachelor's Degree in Business Commerce – Accounting Option from Kibabii University.</li>
+          <li>Bachelor&apos;s Degree in Business Commerce  Accounting Option from Kibabii University.</li>
         </ul>
         <p>
           <strong>Professional Experience:</strong>
         </p>
         <ul>
           <li>Former Records Management Officer at the National Government Constituency Development Fund, Kabuchai Constituency.</li>
-          <li>Served as a Research Assistant at the National Taxpayers’ Association.</li>
+          <li>Served as a Research Assistant at the National Taxpayers Association.</li>
         </ul>
       </div>
     ),
